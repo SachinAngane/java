6c653f10f5bf7d68e36769a70ab83335a852106d
SachinAngane <52366056+SachinAngane@users.noreply.github.com>
2023-04-02 10:29:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !!! Jenkin Java Program !!!");
+		System.out.println("Hello World !!! Jenkin Java Program !!! Update for Jenkin !!!!");
 	}
 }
